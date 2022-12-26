d3f1e7364d54b2c6b426beb237fc0828a21bd7c4
weix <414723943@qq.com>
2022-12-26 16:06:43 +0800

    feat:初始化plm模块

--- yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/YudaoServerApplication.java
@@ -13,7 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author 芋道源码
  */
 @SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${yudao.info.base-package}
-@SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server", "${yudao.info.base-package}.module"})
+@SpringBootApplication(scanBasePackages = {"${yudao.info.base-package}.server"
+        , "${yudao.info.base-package}.module"
+        , "${yudao.info.base-package}.plm"})
 public class YudaoServerApplication {
 
     public static void main(String[] args) {
